238ff71aa04bdf33b1532d72aefd3b960e15bf79
Js-focus <juanca943005>
2022-04-18 12:40:18 -0500

    Se crean rutas y componentes bases

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const NavBar = () => {
+    return (
+        <div>
+            <Link></Link>
+            <Link></Link>
+            <Link></Link>
+            <Link></Link>
+        </div>
+    );
+};
+
+export default NavBar;
