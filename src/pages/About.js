d8416926952d414d67398fecf2bcbcd0be3661c4
Js-focus <juanca943005>
2022-05-03 10:16:33 -0500

    cambios de diseño en about

--- src/pages/About.js
+++ src/pages/About.js
@@ -15,32 +15,47 @@ const About = () => {
             <strong>
                 This is me:
             </strong>
-            <img src={cat2} alt="" width='400' height='400'/>
-            <p>
-                Yeah i'm a cat, but i now in my human fase and i 
-                currently look more like this: 
-            </p>
-            <img src={my1} alt="" width='400' height='400'/>
-            <strong>
-                Yap i have also dog friends, i'm grew up in a small town on the forest in Mexico.
-                I think this explain better:
-            </strong>
-            <img src={cascade} alt="" width='400' height='600' />
-            <strong>
-                I love do trekking i think is the better way to restart my mind and be focused,
-                i only need my backpack and my sombrero to be in the word that i love.
-            </strong>
-            <img src={tunel} alt="" width='400' height='400' />
-            <strong>
-                I started coding in the university for one class and for me was so interesting 
-                does programs in visual basic, my live change and i forget everything but i'm 
-                most focused and i think  my past creat my strong mind fulness, i love my life 
-                when i write and find solutions for doing in code.  
-            </strong>
-            <strong>
-                Don't take your look of my way i grow day by day. See ya and don't forget to be in contact.
-            </strong>
-            <img src={cat1} alt="" width='400' height='400' />
+
+            <section className='section-about'>
+                <img src={cat2} alt="" width='300' height='300'/>
+                <p>
+                    Yeah i'm a cat, but i now in my human fase and i 
+                    currently look more like this: 
+                </p>
+            </section>
+
+            <section className='section-about'>
+                <p>
+                    Yap i have also dog friends, i'm grew up in a small town on the forest in Mexico.
+                    I think this explain better:
+                </p>
+                <img src={my1} alt="" width='300' height='300'/>
+            </section>
+            
+            <section className='section-about'>
+                <img src={cascade} alt="" width='300' height='500' />
+                <p>
+                    I love do trekking i think is the better way to restart my mind and be focused,
+                    i only need my backpack and my sombrero to be in the word that i love.
+                </p>
+            </section>
+            
+            <section className='section-about'>
+                <p>
+                    I started coding in the university for one class and for me was so interesting 
+                    does programs in visual basic, my live change and i forget everything but i'm 
+                    most focused and i think  my past creat my strong mind fulness, i love my life 
+                    when i write and find solutions for doing in code.  
+                </p>
+                <img src={tunel} alt="" width='300' height='300' />
+            </section>
+            
+            <section className='section-about-last'>
+                <p>
+                    Don't take your look of my way i grow day by day. See ya and don't forget to be in contact.
+                </p>
+                <img src={cat1} alt="" width='300' height='300' />
+            </section>
         </div>
     );
 };
